e16eacb60e522a492bad8f0e96ede32ef2487cbf
hwkwkwk1k <dcplayz511@gmail.com>
2025-01-07 23:44:15 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,13 +2,13 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7747598023:AAGfi-m_9gw2L_4McP6z62OjGlbc-AavNVY")
+BOT_TOKEN=("7808154142:AAFKhag-6xJobkAOqrn351BLauVhMOeUXSM")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("BGMIDDOS1245")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1724246173")
+ADMIN_IDS=("7279164468")
 
 
 
